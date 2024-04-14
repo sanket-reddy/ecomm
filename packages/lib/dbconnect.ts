b06868a3156d27232a2ace91944e7c788d1f801c
Sanketh Pathuri <133153221+sanket-reddy@users.noreply.github.com>
2024-04-14 17:21:10 +0530

    jber

--- packages/lib/dbconnect.ts
+++ packages/lib/dbconnect.ts
@@ -7,7 +7,8 @@ export async function ensureDbConnected() {
   } else {
     alreadyDone = true;
     await mongoose.connect(
-      "mongodb+srv://sanketh:G3ivLcBrY6s2BaMu@cluster0.3xcb2wu.mongodb.net/"
+      "mongodb+srv://site-1:passwordofsite-1@cluster0.agwik1p.mongodb.net/"
+      // "mongodb+srv://sanketh:G3ivLcBrY6s2BaMu@cluster0.3xcb2wu.mongodb.net/"
     );
   }
 }
