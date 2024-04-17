99898e841eab5393576ad2873eaf07640f16c253
Sanketh Pathuri <133153221+sanket-reddy@users.noreply.github.com>
2024-04-17 13:41:58 +0530

    db issue resolved

--- packages/lib/dbconnect.ts
+++ packages/lib/dbconnect.ts
@@ -7,8 +7,7 @@ export async function ensureDbConnected() {
   } else {
     alreadyDone = true;
     await mongoose.connect(
-      "mongodb+srv://site-1:passwordofsite-1@cluster0.agwik1p.mongodb.net/"
-      // "mongodb+srv://sanketh:G3ivLcBrY6s2BaMu@cluster0.3xcb2wu.mongodb.net/"
+      "mongodb+srv://sanketh:G3ivLcBrY6s2BaMu@cluster0.3xcb2wu.mongodb.net/"
     );
   }
 }
